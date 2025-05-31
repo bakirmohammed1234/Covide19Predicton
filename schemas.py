ca1046cc854cdbd3661c950c289f7567d5689496
Bakir Mohammed <bakirmohammed018@gmail.com>
2025-05-31 23:48:43 +0100

    ajouter patient class pydanic

--- schemas.py
+++ schemas.py
@@ -4,3 +4,25 @@ class StudentCreate(BaseModel):
     name: str
     age: int
     email: str
+
+class Patient(BaseModel):
+    USMER: int
+    MEDICAL_UNIT: int
+    SEX: int
+    PATIENT_TYPE: int
+    INTUBED: int
+    PNEUMONIA: int
+    AGE: int
+    PREGNANT: int
+    DIABETES: int
+    COPD: int
+    ASTHMA: int
+    INMSUPR: int
+    HIPERTENSION: int
+    OTHER_DISEASE: int
+    CARDIOVASCULAR: int
+    OBESITY: int
+    RENAL_CHRONIC: int
+    TOBACCO: int
+    CLASIFFICATION_FINAL: int
+    ICU: int
